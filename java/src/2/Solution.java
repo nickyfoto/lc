9ed054ae805b03bcdc89c46c3f309d08a451a2e2
Qiang Huang <nickyfoto@gmail.com>
2020-01-26 10:21:03 -0500

    java 2

---
+++ java/src/2/Solution.java
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (32.61%)
+ * Likes:    6866
+ * Dislikes: 1774
+ * Total Accepted:    1.2M
+ * Total Submissions: 3.6M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order and each of their nodes
+ * contain a single digit. Add the two numbers and return it as a linked list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        int v1, v2, val;
+        ListNode root = new ListNode(0);
+        ListNode n = root;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            v1 = 0;
+            v2 = 0;
+            if (l1 != null) {
+                v1 = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                v2 = l2.val;
+                l2 = l2.next;
+            }
+            val = (v1 + v2 + carry) % 10;
+            carry = (v1 + v2 + carry) / 10;
+            n.next = new ListNode(val);
+            n = n.next;
+        }
+        return root.next;
+    }
+}
+// @lc code=end
