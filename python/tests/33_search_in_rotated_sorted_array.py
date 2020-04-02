1771d3d2db92db4c0204565b3f8fdbac66c6238e
Qiang Huang <nickyfoto@gmail.com>
2020-04-02 12:07:12 -0500

    33 bs

--- python/tests/33_search_in_rotated_sorted_array.py
+++ python/tests/33_search_in_rotated_sorted_array.py
@@ -139,4 +139,23 @@ class Solution:
                 h = mid
         return -1
 
+    def search(self, nums, target):
+        res = -1
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                res = mid
+                break
+            elif nums[l] <= nums[mid]: # use <= because l can be equal to mid
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return res
 # @lc code=end
