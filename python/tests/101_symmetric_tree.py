0202b20acff87e0ac657c0362c845b8185d5daa0
Qiang Huang <nickyfoto@gmail.com>
2020-04-12 13:18:17 -0500

    101

---
+++ python/tests/101_symmetric_tree.py
@@ -0,0 +1,61 @@
+#
+# @lc app=leetcode id=101 lang=python3
+#
+# [101] Symmetric Tree
+#
+# https://leetcode.com/problems/symmetric-tree/description/
+#
+# algorithms
+# Easy (43.59%)
+# Total Accepted:    415.8K
+# Total Submissions: 951.9K
+# Testcase Example:  '[1,2,2,3,4,4,3]'
+#
+# Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+# around its center).
+# 
+# For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+# 
+# 
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   2
+# ⁠/ \ / \
+# 3  4 4  3
+# 
+# 
+# 
+# 
+# But the following [1,2,2,null,3,null,3] is not:
+# 
+# 
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   2
+# ⁠  \   \
+# ⁠  3    3
+# 
+# 
+# 
+# 
+# Note:
+# Bonus points if you could solve it both recursively and iteratively.
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # def isSymmetric(self, root: TreeNode) -> bool:
+    def isSymmetric(self, root):
+
+        def isMirror(a, b):
+            if a is None and b is None: return True
+            if a is None or b is None: return False
+            return a.val == b.val and isMirror(a.left, b.right) and isMirror(a.right, b.left)
+        
+        return isMirror(root, root)
